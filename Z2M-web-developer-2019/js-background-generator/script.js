668ce4968a23383b390451964619dfe96a38eade
Kyle Pence <kpence@Kyles-MacBook-Pro.local>
2019-08-20 21:27:00 -0500

    2019_08_20__21_27_00

--- Z2M-web-developer-2019/js-background-generator/script.js
+++ Z2M-web-developer-2019/js-background-generator/script.js
@@ -1,6 +1,7 @@
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
+var random = document.querySelector(".random");
 var body = document.getElementById("gradient");
 
 function setGradient() {
@@ -14,6 +15,37 @@ function setGradient() {
 	css.textContent = body.style.background + ";";
 }
 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+          color += letters[Math.floor(Math.random() * 16)];
+        }
+    return color;
+}
+
+function randomize() {
+  var c1 = getRandomColor();
+  var c2 = getRandomColor();
+  color1.value = c1;
+  color2.value = c2;
+  setGradient();
+}
+
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+random.addEventListener("click", randomize);
+
+
+// get colors from background, assign them to the buttons
+color1.value = "#ff0000";
+color2.value = "#ffff00";
+
+css.textContent = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
